24b17d4f08b4496ca94351e4417d362fa14bc81d
RimAaloi <aaloirim@example.com>
2025-10-18 02:35:59 +0200

    update inventory service

---
+++ inventory-service/src/test/java/org/sid/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package org.sid.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
