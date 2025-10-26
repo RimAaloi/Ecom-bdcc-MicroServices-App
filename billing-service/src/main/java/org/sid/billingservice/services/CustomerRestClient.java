39f3ac3179a18f75be039d6a9596c5a3952e227d
RimAaloi <aaloirim@example.com>
2025-10-26 23:42:25 +0100

    update

--- billing-service/src/main/java/org/sid/billingservice/services/CustomerRestClient.java
+++ billing-service/src/main/java/org/sid/billingservice/services/CustomerRestClient.java
@@ -3,11 +3,15 @@ package org.sid.billingservice.services;
 import lombok.Value;
 import org.sid.billingservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
-    @GetMapping(path="/customers/{id}")
+    @GetMapping(path="/api/customers/{id}")
     Customer FindCustomerById(@PathVariable long id);
+
+    @GetMapping(path="/api/customers")
+    PagedModel<Customer> AllCustomers();
 }
